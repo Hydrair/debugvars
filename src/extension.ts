326276723e61bb19250e4438aa620d5ddc312907
Michael Kainz <michael.kainz@open-xchange.com>
2022-10-14 07:49:07 +0200

    edit commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { LaunchProvider } from './treeProvider';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -13,13 +14,23 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extendeddebugging.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ExtendedDebugging!');
-	});
+	// let disposable = vscode.commands.registerCommand('extendeddebugging.helloWorld', () => {
+	// 	// The code you place here will be executed every time your command is executed
+	// 	// Display a message box to the user
+	// 	vscode.window.showInformationMessage('Hello World from ExtendedDebugging!');
+	// });
 
-	context.subscriptions.push(disposable);
+	// context.subscriptions.push(disposable);
+  
+  
+  const rootPath = (vscode.workspace.workspaceFolders && (vscode.workspace.workspaceFolders.length > 0)) ? vscode.workspace.workspaceFolders[0].uri.fsPath : undefined;
+  const vsPath = rootPath + '/.vscode/launch.json';
+  console.log(rootPath,vsPath);
+  
+  const provider = new LaunchProvider(vsPath);
+  vscode.window.registerTreeDataProvider('extended', provider);
+  vscode.commands.registerCommand('extendeddebugging.editValue',(e) =>provider.editValue(e));
+
 }
 
 // This method is called when your extension is deactivated
---
+++ src/treeProvider.ts
@@ -0,0 +1,106 @@
+import path = require('node:path');
+import fs = require('node:fs');
+import * as vscode from 'vscode';
+
+export class LaunchProvider implements vscode.TreeDataProvider<Option> {
+
+	private _onDidChangeTreeData: vscode.EventEmitter<Option | undefined | void> = new vscode.EventEmitter<Option | undefined | void>();
+	readonly onDidChangeTreeData: vscode.Event<Option | undefined | void> = this._onDidChangeTreeData.event;
+  inputs: any;
+
+	constructor(private launchJSON: string ) {
+	}
+
+	refresh(): void {
+		this._onDidChangeTreeData.fire();
+	}
+
+	getTreeItem(element: Option): vscode.TreeItem {
+		return element;
+	}
+
+	getChildren(element?: Option): Thenable<Option[]> {
+		if (!this.launchJSON) {
+			vscode.window.showInformationMessage('No dependency in empty workspace');
+			return Promise.resolve([]);
+		}
+
+		if (element) {
+      return Promise.resolve([]);
+    } else {
+      return Promise.resolve(this.getInputsInLaunchJson());
+    }
+	}
+
+  private getInputsInLaunchJson(){
+    const launchJSONPath = this.launchJSON;
+    if(this.pathExists(launchJSONPath)){
+      // beware of end of line comma
+      this.inputs = this.getLaunchFile().inputs;
+
+      const toOpt = (option: {
+        default: string;id:string,type:string
+}) =>{
+        return new Option(option.id,option.default,option.type,vscode.TreeItemCollapsibleState.None,{
+          title: 'title',
+          command: "extendeddebugging.editValue",
+          arguments: [this]
+        },);
+      };
+
+      const opts = this.inputs.map((input: { id: string;  default: string;type: string; }) => toOpt(input));
+
+      return opts;
+    } else {
+      return [];
+    }
+  }
+
+  private getLaunchFile(){
+    return JSON.parse(fs.readFileSync(this.launchJSON,{encoding: "utf-8"}));
+  }
+
+	private pathExists(p: string): boolean {
+		try {
+			fs.accessSync(p);
+		} catch (err) {
+			return false;
+		}
+
+		return true;
+	}
+
+  public async editValue(element: Option){
+    const newValue = await vscode.window.showInputBox({
+      placeHolder: element.value,
+      prompt: `Enter a new value for ${element.label}`,
+    });
+    if( newValue !== undefined ){
+      const editedFile = this.getLaunchFile();
+      editedFile.inputs.find((e: { id: string; }) => e.id === element.label).default = newValue;
+      fs.writeFileSync(this.launchJSON,JSON.stringify(editedFile,null,"\t"),{
+        encoding: 'utf-8',
+      });
+      this.refresh();
+    }
+  }
+}
+
+export class Option extends vscode.TreeItem {
+
+	constructor(
+		public readonly label: string,
+    public  value: string,
+		private readonly inputType: string,
+		public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+		public readonly command?: vscode.Command
+	) {
+   
+		super(label, collapsibleState);
+
+		this.tooltip = `${this.label}`;
+		this.description = `${value}`;
+	}
+
+	contextValue = 'option';
+}
